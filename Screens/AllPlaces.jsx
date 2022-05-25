a17b09c35940b208889e611e17602a7f75e7cdca
James Pesenti <you@example.com>
2022-05-25 17:00:46 -0400

    initial commit

---
+++ Screens/AllPlaces.jsx
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View, } from 'react-native'
+import PlacesList from "../Components/Places/PlacesList"
+
+
+function AllPlaces() {
+  return (
+    <PlacesList />
+
+  )
+}
+
+export default AllPlaces
